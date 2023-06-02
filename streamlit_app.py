1c1e906ec2d89f18b55700f6a937b3eabf6ba2bd
pwarade-2000 <135302969+pwarade-2000@users.noreply.github.com>
2023-06-02 11:49:37 +0530

    Update streamlit_app.py
    Added new text line with Emoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,11 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('🥣Breakfast Menu')
-streamlit.text('🥗Omega 3 & Blueberry Oatmeal')
-streamlit.text('🐔Kale, Spanish & Rocket Smoothie')
-streamlit.text('🥑🍞Hard-Boiled Free-Range Egg')
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spanish & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
