d14215a2863a3ccd34b8aec698d1cd0402e929b7
pwarade-2000 <135302969+pwarade-2000@users.noreply.github.com>
2023-06-02 11:47:05 +0530

    Update streamlit_app.py
    Added Emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spanish & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('🥣Breakfast Menu')
+streamlit.text('🥗Omega 3 & Blueberry Oatmeal')
+streamlit.text('🐔Kale, Spanish & Rocket Smoothie')
+streamlit.text('🥑🍞Hard-Boiled Free-Range Egg')
